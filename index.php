b7bb9ee78e06fa20443a2545ccc323af0cf3f404
RezaRiyaldi <rezamoza3@gmail.com>
2022-03-08 15:17:33 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+  $judul = "Selamat Tinggal";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo $judul; ?></title>
+</head>
+<body>
+  <!-- <?php
+    echo "<h1>Hai bro $judul</h1>";
+  ?>
+
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam provident necessitatibus distinctio reprehenderit illo sequi <?php echo $judul; ?> laborum porro magni autem recusandae nobis unde quae nostrum corrupti, explicabo officiis eos nisi cupiditate!</p> -->
+
+
+
+  <?php 
+
+  // String
+  $text = "Ini string";
+  var_dump($text);
+  echo "<br>" . $text;
+
+  // int
+  $x = 10;
+  $y = 5;
+  // var_dump($x + $y);
+  ?>
+</body>
+</html>
